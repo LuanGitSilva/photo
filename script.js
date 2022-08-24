fdb5b322075d2f84a048b93a4f957fca3a956f12
Luan Silva <luan_barbosa_da_silva@outlook.com>
2022-08-24 10:49:30 -0300

    Update

--- script.js
+++ script.js
@@ -4,14 +4,13 @@ const topNav = document.querySelector('.top-nav');
 showBtn.addEventListener('click', function() {
     if(topNav.classList.contains('showNav')) {
         topNav.classList.remove('showNav');
+        document.querySelector('.brand-and-navBtn').style.background = 'transparent';
     } else {
         topNav.classList.add('showNav');
+        document.querySelector('.brand-and-navBtn').style.background = 'black';
     }
 });
 
 function myFunction(x) {
     document.querySelector(".container").classList.toggle("change");
 } 
-
-// /* lightbox */
-// var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
